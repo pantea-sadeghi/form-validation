be11b42a144f13e1a944c6fc0f0a1a8e39e3ba43
PanteaSadeghi <panteamirzasadeghi2001@gmail.com>
2024-08-02 04:37:21 +0430

    Adding an error in the phone number section

--- accets/script.js
+++ accets/script.js
@@ -4,16 +4,34 @@ var emailError = document.getElementById('email-error');
 var messageError = document.getElementById('message-error');
 var submitError = document.getElementById('submit-error');
 
-function validateName(){
+function validateName() {
     var name = document.getElementById('contact-name').value;
-    if(name.length == 0){
+    if (name.length == 0) {
         nameError.innerHTML = 'Name is required';
         return false;
     }
-    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
         nameError.innerHTML = 'Write full name';
         return false;
     }
     nameError.innerHTML = '<i class="fa fa-check-circle" aria-hidden="true"></i>';
+    return true;
+}
+
+function validatePhone() {
+    var phone = document.getElementById('contact-phone').value;
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone No is required';
+        return false;
+    }
+    if (phone.length !== 10) {
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false;
+    }
+    if (!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = 'Only digits please';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa fa-check-circle" aria-hidden="true"></i>';
     return true;
 }
